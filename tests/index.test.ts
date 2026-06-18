78875a7de7ca789475c3cc29fa4ed0e5b3229f66
Cezar Augusto <boss@cezaraugusto.net>
2026-06-17 23:46:29 -0300

    Apply eslint-config-auditor formatting and lint rules

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,7 +3,7 @@ import {expect, test, describe} from 'vitest'
 import waterfoxLocation from '../src/index'
 
 describe('waterfox-location module', () => {
-  test('returns string or null', () => {
+  it('returns string or null', () => {
     const res = waterfoxLocation()
 
     expect(typeof res === 'string' || res === null).toBe(true)
